77bc77278434d1f21a445b88a58d8c53f0016aec
DP1EBerzins <>
2021-06-01 11:26:03 +0000

    Add some pages

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ app = Flask('app')
 
 
 @app.route('/')
-def index():
-  return render_template("index.html")
+def Homepage():
+  return render_template("Homepage.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/Logged-In')
+def LoggedInPage():
+  return render_template("Ielogojies_Lapa.html")
+
+@app.route('/Admin-Page')
+def AdminPage():
+  return render_template("Admin_Homepage.html")
+
+@app.route('/Rezervēšana')
+def Rezervet():
+  return render_template("rezervacijas.html")
+
+@app.route('/Admin-Rediģēšana')
+def Rediget():
+  return render_template("Admin_Viesnīcu_Rediģēšana.html")
 
 
 app.run(host='0.0.0.0', port=8080)
