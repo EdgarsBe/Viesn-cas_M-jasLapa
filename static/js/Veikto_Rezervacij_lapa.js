c5eafb65b1777940ded18c8224fa9029580d7e26
DP1EBerzins <>
2021-06-08 11:54:58 +0000

    Javascript fixes

--- static/js/Veikto_Rezervacij_lapa.js
+++ static/js/Veikto_Rezervacij_lapa.js
@@ -1,24 +1,5 @@
-const POP_UP1 = document.getElementById('Pop_up1');
-const POP_UP2 = document.getElementById('Pop_up2');
 const POP_UP3 = document.getElementById('rezervacijasRedigesana')
 const POP_UP4 = document.getElementById('statistikasEksportesana')
-
-document.getElementById('button2').addEventListener('click', () => {
-    POP_UP1.style.display = 'block';
-})
-document.getElementById('IeietPoga').addEventListener('click', () => {
-    POP_UP1.style.display = 'none'
-})
-document.getElementById('ReģPoga1').addEventListener('click', () => {
-    POP_UP1.style.display = 'none'
-})
-
-document.getElementById('button3').addEventListener('click', () => {
-    POP_UP2.style.display = 'block';
-})
-document.getElementById('ReģPoga2').addEventListener('click', () => {
-    POP_UP2.style.display = 'none'
-})
 
 document.querySelectorAll('#redigesanasIkona').forEach(item => {
     item.addEventListener('click', event => {
